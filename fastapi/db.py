083fdeb0540ac6494eae1599174fbd3f7a4fab0e
Fabrice Fourel <ffourel@gmail.com>
2025-10-17 16:35:57 +0200

    Add json outputs

--- fastapi/db.py
+++ fastapi/db.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2.extras import sql
+import json
 
 # Importer la fonction get_connection depuis config.py
 from config import get_connection
@@ -10,24 +11,10 @@ from config import get_connection
 def read_all_items():
     query = "SELECT * FROM portfolio"
     with get_connection() as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
             cur.execute(query)
             rows = cur.fetchall()
-            items = []
-            for row in rows:
-                item = {
-                    "cryptoid": row[0],
-                    "cryptoname": row[1],
-                    "coingeckoid": row[2],
-                    "alert1": row[3],
-                    "alert2": row[4],
-                    "alert3": row[5],
-                    "lastprice": row[6],
-                    "amount": row[7],
-                    "total": row[8],
-                }
-                items.append(item)
-            return items
+            return json.dumps(rows, indent=4, default=str)
 
 # Fonction pour mettre à jour un élément
 def update_item(cryptoid, **kwargs):

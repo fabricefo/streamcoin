e7d56656d21ee23f58aef16c2e2f9c42af92b0cc
Fabrice Fourel <ffourel@gmail.com>
2025-10-17 16:14:18 +0200

    Clean code

--- streamlit/config.py
+++ streamlit/config.py
@@ -1,20 +1,7 @@
-import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from dotenv import load_dotenv
-
-
-# Charger les variables d'environnement depuis un fichier .env
-load_dotenv()
-
 
 # Récupérer les informations de connexion depuis les variables d'environnement
-# DB_HOST = os.getenv("DB_HOST", "localhost")
-# DB_NAME = os.getenv("DB_NAME", "test")
-# DB_USER = os.getenv("DB_USER", "test")
-# DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-# DB_PORT = os.getenv("DB_PORT", "5432")
-
 DB_HOST = "postgres"
 DB_NAME = "crypto"
 DB_USER = "api"
